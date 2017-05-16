9a5574a37f68586f1bd98688c60007ad87837a3e
Mike Wajda <mewajda@gmail.com>
2017-05-16 14:51:59 -0500

    appveyor nuget work

--- PayJunction/Properties/AssemblyInfo.cs
+++ PayJunction/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PayJunction")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("API Library for PayJunction API")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Nixtus, LLC")]
 [assembly: AssemblyProduct("PayJunction")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
